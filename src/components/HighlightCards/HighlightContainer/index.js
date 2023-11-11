ceccc6f171b94d75f123dd18ff607a66d8e3151a
Noel <iamnoelsto@gmail.com>
2023-11-11 13:00:13 +0100

    Style Highlight Card with container

---
+++ src/components/HighlightCards/HighlightContainer/index.js
@@ -0,0 +1,7 @@
+import { StyledHighlightContainer } from "./styles";
+
+const HighlightContainer = ({ children }) => {
+  return <StyledHighlightContainer>{children}</StyledHighlightContainer>;
+};
+
+export default HighlightContainer;
---
+++ src/components/HighlightCards/HighlightContainer/styles.js
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components";
+
+export const StyledHighlightContainer = tw.div`
+mt-5
+grid
+grid-cols-1
+gap-5
+sm:grid-cols-4
+`;
